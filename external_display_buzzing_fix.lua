0aaceedaa9e3398c8ed6aced3c82380a80f919bf
VincentStark <471022+VincentStark@users.noreply.github.com>
2023-05-10 20:52:56 -0500

    Make it more robust, to restore the correct brightness.

--- external_display_buzzing_fix.lua
+++ external_display_buzzing_fix.lua
@@ -1,16 +1,9 @@
 -- Fix external display buzzing if not 100% brightness on wake up
-local brightnessKey = "externalDisplayPrevBrightness"
-
 watcher = hs.caffeinate.watcher.new(function(eventType)
-    if eventType == hs.caffeinate.watcher.screensDidSleep then
-        -- Save current brightness
-        local currentBrightness = hs.execute("/opt/homebrew/bin/m1ddc get luminance")
-        hs.settings.set(brightnessKey, currentBrightness)
-
-    elseif eventType == hs.caffeinate.watcher.screensDidWake then
+    if eventType == hs.caffeinate.watcher.screensDidWake then
         -- Restore previous brightness after a delay
-        hs.timer.doAfter(2.0, function()
-            local prevBrightness = hs.settings.get(brightnessKey)
+        hs.timer.doAfter(3.0, function()
+            local prevBrightness = hs.execute("/opt/homebrew/bin/m1ddc get luminance")
             hs.execute("/opt/homebrew/bin/m1ddc set luminance 100")
             if tonumber(prevBrightness) < 100 then
                 hs.execute("/opt/homebrew/bin/m1ddc set luminance " .. prevBrightness)

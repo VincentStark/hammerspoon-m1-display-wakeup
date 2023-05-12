54814a8675138a3c153f2397f8f6d656c1797ce3
VincentStark <471022+VincentStark@users.noreply.github.com>
2023-05-11 19:00:38 -0500

    Improve to avoid situations when brightness sets to 0 after wake up.

--- external_display_buzzing_fix.lua
+++ external_display_buzzing_fix.lua
@@ -2,11 +2,13 @@
 watcher = hs.caffeinate.watcher.new(function(eventType)
     if eventType == hs.caffeinate.watcher.screensDidWake then
         -- Restore previous brightness after a delay
-        hs.timer.doAfter(3.0, function()
+        hs.timer.doAfter(2.5, function()
             local prevBrightness = hs.execute("/opt/homebrew/bin/m1ddc get luminance")
             hs.execute("/opt/homebrew/bin/m1ddc set luminance 100")
             if tonumber(prevBrightness) < 100 then
-                hs.execute("/opt/homebrew/bin/m1ddc set luminance " .. prevBrightness)
+                hs.timer.doAfter(0.5, function()
+                    hs.execute("/opt/homebrew/bin/m1ddc set luminance " .. prevBrightness)
+                end)
             end
         end)
     end

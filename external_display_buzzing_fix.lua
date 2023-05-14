1b13f0998d045ef791a99900c5221f561452550b
VincentStark <471022+VincentStark@users.noreply.github.com>
2023-05-14 01:04:08 -0500

    Improve robustness of the changes.

--- external_display_buzzing_fix.lua
+++ external_display_buzzing_fix.lua
@@ -2,13 +2,15 @@
 watcher = hs.caffeinate.watcher.new(function(eventType)
     if eventType == hs.caffeinate.watcher.screensDidWake then
         -- Restore previous brightness after a delay
-        hs.timer.doAfter(2.5, function()
+        hs.timer.doAfter(2.0, function()
             local prevBrightness = hs.execute("/opt/homebrew/bin/m1ddc get luminance")
             hs.execute("/opt/homebrew/bin/m1ddc set luminance 100")
-            if tonumber(prevBrightness) < 100 then
-                hs.timer.doAfter(0.5, function()
-                    hs.execute("/opt/homebrew/bin/m1ddc set luminance " .. prevBrightness)
-                end)
+            prevBrightness = tonumber(prevBrightness)
+            if prevBrightness < 100 then
+                if prevBrightness < 60 then
+                    prevBrightness = 60
+                end
+                hs.execute("/opt/homebrew/bin/m1ddc set luminance " .. prevBrightness)
             end
         end)
     end
